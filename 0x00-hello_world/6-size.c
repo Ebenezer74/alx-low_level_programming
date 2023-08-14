0263955c3400aab9a6f453e7ba767a4c1060a0c5
Ebenezer <ebenezeraikins74@gmail.com>
2023-08-13 17:27:42 -0700

    Added new c scripts

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *
+ * Entry point of main 
+ *
+ *Return:Always 0(Success)
+ */
+int main(void)
+{     printf("Size of a char %d byte(s)\n", Sizeof(char));
+      printf("Size of an int %d byte(S)\n", Sizeof(int));
+      printf("Size of a long long int %d byte(s)\n", Sizeof(long long int));
+      printf("Size of a long int  %d byte(s)\n", Sizeof(longint));
+      printf("Size of a float %D bytes(s)\n", Sizeof(float));
+
+      return(0);
+}

8e154d88df39743142e4d8580a34ab8645baba6e
Ebenezer <ebenezeraikins74@gmail.com>
2023-08-16 13:06:06 -0700

    New 0-putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * main - This function prints using _putchar method
- * Return: 0 (Success)
+ *main - This function prints a character using _putchar method
+ *R eturn: 0 (Success)
  */
 int main(void)
 {
-	char name = 'A';
-
-	putchar(name);
-	putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }

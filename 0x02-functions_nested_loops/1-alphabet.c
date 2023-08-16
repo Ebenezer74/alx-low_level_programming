c95b1e3e7378991ce925c4cf891702f9dba7d08e
Ebenezer <ebenezeraikins74@gmail.com>
2023-08-16 15:28:35 -0700

    2nd c program

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * This function print all the alphabets
+ * Return: void (success)
+ */
+void print_alphabet(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}

43b6ef30fb7c2fbf9837e06da6c48e0aae502d8d
Ebenezer <ebenezeraikins74@gmail.com>
2023-08-21 22:04:40 -0700

    edited program file 8

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -1,11 +1,13 @@
 #include "main.h"
+
 /**
- * main - check the code
+ * main - check the code fo
  * Return: Always 0.
  */
 int main(void)
 {
 int array[5];
+
 array[0] = 98;
 array[1] = 402;
 array[2] = -198;
